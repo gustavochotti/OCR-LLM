c48ff3894783a38e662c2427bda6ee241753ca34
gustavochotti <106937773+gustavochotti@users.noreply.github.com>
2025-06-06 19:03:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from llm_handler import generate_response, load_model
 
 if __name__ == "__main__":
   tokenizer, model = load_model()
-  image_path = "exemplo.png"
+  image_path = "IMAGE.png"  # or the image path
   text = read_image(image_path)
-  question = f"Responda a seguinte pergunta:\n{text}"
+  question = f"Summarize this text:\n{text}"
 
   generate_response(question, tokenizer, model)
