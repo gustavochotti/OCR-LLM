f93df2f2edd1b402d94354c1d76f77ec4d5e7611
gustavochotti <106937773+gustavochotti@users.noreply.github.com>
2025-06-07 18:22:35 -0300

    Update torch_utils.py

--- torch_utils.py
+++ torch_utils.py
@@ -1,13 +1,13 @@
+# torch_utils.py
 import torch
 
 def get_best_dtype() -> torch.dtype:
-  if not torch.cuda.is_available():
-    return torch.float32
-  name = torch.cuda.get_device_name(0).lower()
-  if any(x in name for x in ["a100", "h100", "gaudi"]):
-    return torch.bfloat16
-  
-  return torch.float16
+    if not torch.cuda.is_available():
+        return torch.float32
+    name = torch.cuda.get_device_name(0).lower()
+    if any(x in name for x in ["a100", "h100", "gaudi"]):
+        return torch.bfloat16
+    return torch.float16
 
 def get_device() -> str:
-  return "cuda" if torch.cuda.is_available() else "cpu"
+    return "cuda" if torch.cuda.is_available() else "cpu"
